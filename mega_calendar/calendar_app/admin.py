0b00bb6ff6b46cd600ab8bf440d362a9fe965f3a
Andrey Smakotin <slamfromm@gmail.com>
2022-03-28 20:39:05 +0300

    added notification model

--- mega_calendar/calendar_app/admin.py
+++ mega_calendar/calendar_app/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from calendar_app.models import User
+from calendar_app.models import User, Event, Notification
 
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'email', 'city')
+    list_display = ('id', 'username', 'first_name', 'last_name', 'email', 'city', 'is_superuser', 'is_active')
+    list_editable = ('is_active',)
+    list_display_links = ('id', 'username', 'email')
+
+
+admin.site.register(Event)
+admin.site.register(Notification)
--- mega_calendar/calendar_app/models.py
+++ mega_calendar/calendar_app/models.py
@@ -10,3 +10,18 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['username', 'city']
 
 
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    event_date = models.DateField()  # format 'YYYY-MM-DD'
+    start_time = models.TimeField(default='00:00')
+    end_time = models.TimeField(default='23:59')
+    created_at = models.DateTimeField(auto_now_add=True)
+    notification = models.ForeignKey('Notification', on_delete=models.CASCADE)
+
+
+class Notification(models.Model):
+    notification = models.DurationField(default='01:00:00')  # [DD] [[HH:]MM:]ss[.uuuuuu] format.
+
+    def __str__(self):
+        return self.notification
