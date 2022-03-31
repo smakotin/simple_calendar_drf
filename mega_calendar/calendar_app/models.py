358dec5874f0ab5ef6a3881609d17c515a4ed122
Andrey Smakotin <slamfromm@gmail.com>
2022-03-31 20:17:46 +0300

    default notification

--- mega_calendar/calendar_app/models.py
+++ mega_calendar/calendar_app/models.py
@@ -10,22 +10,41 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['username']
 
 
+class Notification(models.Model):
+    notification = models.DurationField(default='01:00:00')  # [DD] [[HH:]MM:]ss[.uuuuuu] format.
+
+    @classmethod
+    def get_default_notification(cls):
+        obj, created = cls.objects.get_or_create(notification='01:00:00')
+        return obj.pk
+
+    def __str__(self):
+        return str(self.notification)
+
+
 class Event(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=150)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
-    notification = models.ForeignKey('Notification', on_delete=models.CASCADE)
+    notification = models.ForeignKey(
+        Notification,
+        on_delete=models.SET_DEFAULT,
+        default=Notification.get_default_notification
+    )
     official_holiday = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.start_time > self.end_time:
+            self.start_time, self.end_time = self.end_time, self.start_time
+        super().save(*args, **kwargs)
 
 
 class Country(models.Model):
     country = models.CharField(max_length=100, unique=True)
 
-
-class Notification(models.Model):
-    notification = models.DurationField(default='01:00:00')  # [DD] [[HH:]MM:]ss[.uuuuuu] format.
-
     def __str__(self):
-        return self.notification
+        return self.country
+
+
